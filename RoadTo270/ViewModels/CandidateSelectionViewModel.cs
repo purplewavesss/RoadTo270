f40af47e07002edcbfdeb9d38149da2b2cdc8976
Gavin J. Grotegut <gavin.grotegut@gmail.com>
2023-04-03 13:36:06 -0600

    Created basic design for candidate selection view

---
+++ RoadTo270/ViewModels/CandidateSelectionViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Avalonia.Controls;
+using RoadTo270.Models;
+
+namespace RoadTo270.ViewModels;
+
+public class CandidateSelectionViewModel
+{
+    public Dictionary<string, Candidate> PlayableCandidates;
+}
---
+++ RoadTo270/Views/CandidateSelectionView.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace RoadTo270.Views;
+
+public partial class CandidateSelectionView : UserControl
+{
+    public CandidateSelectionView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
